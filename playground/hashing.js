95c73b36b729b2fd374fabc70b65747af8b6515e
sandeep chandrababu <a-sandeepb@SINGTELCDC.COM>
2018-07-09 17:48:55 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -2,15 +2,28 @@
 
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
+var password = '123abc!';
+// bcrypt.genSalt(10, (err,salt)=> {
+//     bcrypt.hash(password,salt, (err, hash)=> {
+//         console.log(hash);
+//     });
+// });
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token,'123abc');
-console.log(decoded);
+var hashedPassword = '$2a$10$lOoPpIeoTrn1kEugI2SkveLEL4wvvWZZ1SIOH7dr/3rHFtv/0J5X2';
+bcrypt.compare('123!',hashedPassword, (err,res)=> {
+    console.log(res);
+});
+
+// var data = {
+//     id:10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token,'123abc');
+// console.log(decoded);
 
 // var message = 'Username3';
 // var hash = SHA256(message).toString();
